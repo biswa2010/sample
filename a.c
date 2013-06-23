dcbd77a445105716a111f946b836dd53b84b8a46
jyotigithub <jyoti@citrusleaf.com>
2013-06-23 08:26:08 +0530

    1stproject

--- a.c
+++ a.c
@@ -1,2 +1,5 @@
+// Goto following link and implement that quiz game. Please try to understand the way it presents output in console(dont try to just copy paste)
 
-//delete 1st line and start programming for helloworld print
+//http://codeincodeblock.blogspot.in/2011/06/mini-project-quiz-in-c.html
+
+//Give your 100%, so that you can finish this as soon as possible
